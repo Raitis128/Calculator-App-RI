2182338b8e534ab0587d5d2738e243e97bd7e6d1
Raitis128 <raitisiziks@gmail.com>
2022-09-11 16:40:48 +0300

    Add percent function

--- app/src/main/java/com/example/calculator_app_ri/MainActivity.java
+++ app/src/main/java/com/example/calculator_app_ri/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     String oldNumber;
-    String operator;
+    String operator = "";
     Boolean isNew = true;
     EditText etNumbers;
 
@@ -27,16 +27,26 @@ public class MainActivity extends AppCompatActivity {
         isNew = false;
         String number = etNumbers.getText().toString();
         switch (view.getId()){
-            case R.id.btnOne: number = number + "1"; break;
-            case R.id.btnTwo: number = number + "2"; break;
-            case R.id.btnThree: number = number + "3"; break;
-            case R.id.btnFour: number = number + "4"; break;
-            case R.id.btnFive: number = number + "5"; break;
-            case R.id.btnSix: number = number + "6"; break;
-            case R.id.btnSeven: number = number + "7"; break;
-            case R.id.btnEight: number = number + "8"; break;
-            case R.id.btnNine: number = number + "9"; break;
-            case R.id.btnZero: number = number + "0"; break;
+            case R.id.btnOne: number = number + "1";
+                break;
+            case R.id.btnTwo: number = number + "2";
+                break;
+            case R.id.btnThree: number = number + "3";
+                break;
+            case R.id.btnFour: number = number + "4";
+                break;
+            case R.id.btnFive: number = number + "5";
+                break;
+            case R.id.btnSix: number = number + "6";
+                break;
+            case R.id.btnSeven: number = number + "7";
+                break;
+            case R.id.btnEight: number = number + "8";
+                break;
+            case R.id.btnNine: number = number + "9";
+                break;
+            case R.id.btnZero: number = number + "0";
+                break;
 
             case R.id.btnDot:
                 if (dotIsPresent(number)) {
@@ -60,10 +70,14 @@ public class MainActivity extends AppCompatActivity {
         isNew = true;
         oldNumber = etNumbers.getText().toString();
         switch (view.getId()){
-            case R.id.btnMinus: operator = "-" ; break;
-            case R.id.btnPlus: operator = "+" ; break;
-            case R.id.btnDivide: operator = "/" ; break;
-            case R.id.btnMultiply: operator = "*" ; break;
+            case R.id.btnMinus: operator = "-" ;
+                break;
+            case R.id.btnPlus: operator = "+" ;
+                break;
+            case R.id.btnDivide: operator = "/" ;
+                break;
+            case R.id.btnMultiply: operator = "*" ;
+                break;
         }
     }
 
@@ -71,10 +85,14 @@ public class MainActivity extends AppCompatActivity {
         String newNumber = etNumbers.getText().toString();
         Double result = 0.0;
         switch (operator){
-            case "-": result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber) ; break;
-            case "+": result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber) ; break;
-            case "*": result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) ; break;
-            case "/": result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber) ; break;
+            case "-": result = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber) ;
+                break;
+            case "+": result = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber) ;
+                break;
+            case "*": result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) ;
+                break;
+            case "/": result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber) ;
+                break;
         }
         etNumbers.setText(result+"");
     }
@@ -97,6 +115,30 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }else {
             return false;
+        }
+    }
+
+    public void clickPercent(View view) {
+        if (operator == ""){
+            String number = etNumbers.getText().toString();
+            double temp = Double.parseDouble(number) / 100;
+            number = temp+"";
+            etNumbers.setText(number);
+        }else {
+            String newNumber = etNumbers.getText().toString();
+            Double result = 0.0;
+            switch (operator){
+                case "+": result = Double.parseDouble(oldNumber) + Double.parseDouble(oldNumber) + Double.parseDouble(newNumber) / 100;
+                    break;
+                case "-": result = Double.parseDouble(oldNumber) - Double.parseDouble(oldNumber) + Double.parseDouble(newNumber) / 100 ;
+                    break;
+                case "*": result = Double.parseDouble(oldNumber) * Double.parseDouble(newNumber) / 100;
+                    break;
+                case "/": result = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber) * 100;
+                break;
+            }
+            etNumbers.setText(result+"");
+            operator = "";
         }
     }
 }
