51ec32f7c063a997481e9ba3cf333b0aa6fdc6f2
Raitis128 <raitisiziks@gmail.com>
2022-09-11 17:52:39 +0300

    Fix string bag

--- app/src/main/java/com/example/calculator_app_ri/MainActivity.java
+++ app/src/main/java/com/example/calculator_app_ri/MainActivity.java
@@ -27,60 +27,15 @@ public class MainActivity extends AppCompatActivity {
         isNew = false;
         String number = etNumbers.getText().toString();
         switch (view.getId()){
-            case R.id.btnOne:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "1";
-                    break;
-            case R.id.btnTwo:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "2";
-                    break;
-            case R.id.btnThree:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "3";
-                    break;
-            case R.id.btnFour:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "4";
-                    break;
-            case R.id.btnFive:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "5";
-                    break;
-            case R.id.btnSix:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "6";
-                    break;
-            case R.id.btnSeven:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "7";
-                    break;
-            case R.id.btnEight:
-                number = number + "8";
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                    break;
-            case R.id.btnNine:
-                if (zeroIsFirst(number)){
-                    number = number.substring(1);
-                }
-                number = number + "9";
-                    break;
+            case R.id.btnOne: number = number + "1"; break;
+            case R.id.btnTwo: number = number + "2"; break;
+            case R.id.btnThree: number = number + "3"; break;
+            case R.id.btnFour: number = number + "4"; break;
+            case R.id.btnFive: number = number + "5"; break;
+            case R.id.btnSix: number = number + "6"; break;
+            case R.id.btnSeven: number = number + "7"; break;
+            case R.id.btnEight: number = number + "8"; break;
+            case R.id.btnNine: number = number + "9"; break;
             case R.id.btnZero:
                 if (zeroIsFirst(number) && number.length() == 1){
                     number = "0";
@@ -171,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             number = temp+"";
             etNumbers.setText(number);
         }else {
-            String newNumber = etNumbers.getText().toString();
+            String newNumber = etNumbers.getText().toString() + "";
             Double result = 0.0;
             switch (operator){
                 case "+": result = Double.parseDouble(oldNumber) + Double.parseDouble(oldNumber) + Double.parseDouble(newNumber) / 100;
